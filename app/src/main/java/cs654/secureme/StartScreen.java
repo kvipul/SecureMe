83e43e58c0983ade0d48d6696dfa0e11af6400ef
sunil12738 <sunil12738@gmail.com>
2016-04-03 01:52:22 +0530

    initial work

---
+++ app/src/main/java/cs654/secureme/StartScreen.java
@@ -0,0 +1,79 @@
+package cs654.secureme;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class StartScreen extends ActionBarActivity {
+
+    EditText fname,lname,yourMobile,helpMobile;
+    Button submit;
+    SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+        fname = (EditText) findViewById(R.id.fname);
+        lname = (EditText) findViewById(R.id.lname);
+        yourMobile = (EditText) findViewById(R.id.yourMobile);
+        helpMobile = (EditText) findViewById(R.id.helpMobile);
+        submit = (Button) findViewById(R.id.submit);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String firstName = fname.getText().toString();
+                String lastName = lname.getText().toString();
+                String yMobile = yourMobile.getText().toString();
+                String hMobile = helpMobile.getText().toString();
+
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("fname", firstName);
+                editor.putString("lname", lastName);
+                editor.putString("yourMobile", yMobile);
+                editor.putString("helpMobile", hMobile);
+                editor.commit();
+            }
+        });
+
+
+
+    }
+
+    public void checkDetails(){
+        if(sharedPreferences.contains("fname") && sharedPreferences.contains("lname") && sharedPreferences.contains("yourMobile") && sharedPreferences.contains("helpMobile")){
+            Intent intent = new Intent(this,MainScreen.class);
+            startActivity(intent);
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_start_screen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
